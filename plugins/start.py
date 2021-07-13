2ac0095c1ebac9d7f65c91b07d6f0b7eb3cda12e
Avengers105 <85744853+Avengers105@users.noreply.github.com>
2021-07-13 14:20:47 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -6,9 +6,9 @@ async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
         [InlineKeyboardButton("Channel", url="https://t.me/movielover2021")],
-        [InlineKeyboardButton("Group", url="https://t.me/songdownloading")],
-        [InlineKeyboardButton("Developer 😊", url="https://t.me/Avengerkannan")]
+        [InlineKeyboardButton(
+            "Developer 😊", url="https://t.me/Avengerkannan")]
     ])
-    welcomed = f"Hello <b>{message.from_user.first_name}</b>\nI am a Simple YouTube videos Download bot/n/help for More info"
-    await message.reply_text(welcomed, reply_markup=joinButton
+    welcomed = f"Hello <b>{message.from_user.first_name}</b>\nI am Simple YouTube Download Bot💓\n/help for More info"
+    await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
