f938480835ea7504c9a1e46abc56058915452247
Avengers105 <85744853+Avengers105@users.noreply.github.com>
2021-07-16 18:19:09 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -9,6 +9,6 @@ async def start(client, message):
         [InlineKeyboardButton(
             "𝔻𝕖𝕧𝕖𝕝𝕠𝕡𝕖𝕣 😊", url="https://t.me/Avengerkannan")]
     ])
-    welcomed = f"𝗛𝗲𝗹𝗹𝗼 <b>@{message.from_user.username}</b>\n\n@ytdownloadv2bot 𝐢𝐬 𝐭𝐡𝐞 𝐌𝐨𝐬𝐭 𝐀𝐝𝐯𝐚𝐧𝐜𝐞𝐝 𝐘𝐨𝐮𝐭𝐮𝐛𝐞 𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 𝐁𝐨𝐭😂[📥]()\n\n/help 𝐅𝐨𝐫 𝐌𝐨𝐫𝐞 𝐈𝐧𝐟𝐨\n© @songdownloading"
+    welcomed = f"𝗛𝗲𝗹𝗹𝗼 <b>@{message.from_user.username}</b>\n\n@ytdownloadv2bot 𝐢𝐬 𝐭𝐡𝐞 𝐌𝐨𝐬𝐭 𝐀𝐝𝐯𝐚𝐧𝐜𝐞𝐝 𝐘𝐨𝐮𝐭𝐮𝐛𝐞 𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 𝐁𝐨𝐭😂[📥](https://telegra.ph/file/28f37560abd3099a65971.jpg)\n\n/help 𝐅𝐨𝐫 𝐌𝐨𝐫𝐞 𝐈𝐧𝐟𝐨\n© @songdownloading"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
