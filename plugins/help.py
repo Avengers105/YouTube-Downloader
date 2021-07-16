bff2626e7788bff663d54272172f410ac6c6f2ed
Avengers105 <85744853+Avengers105@users.noreply.github.com>
2021-07-16 18:39:18 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,5 +3,5 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"<b>ഓ വല്ലിയ formalities ഒന്നും എനിക്ക് ഇല്ല,നിനക്ക് വേണ്ടുന്ന ആ YouTube URL ഇങ്ങോട്ട് ഒന്ന് തന്നാൽ മാത്രം മതി🤪..<\b>\n\n<i>Currently Only supports Youtube Single  (No playlist) Just Send Youtube Url<\i>"
+    helptxt = f"<b>ഓ വല്ലിയ formalities ഒന്നും എനിക്ക് ഇല്ല,ഒരു YouTube URL ഇങ്ങോട്ട് ഒന്ന് തന്നാൽ മാത്രം മതി🤪..\n\nCurrently Only supports Youtube Single  (No playlist) Just Send Youtube Url\n@movielover2021"
     await message.reply_text(helptxt)
